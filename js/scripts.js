c020474a6af838b1f70349b99914cab9c4f4e460
Ugonz86 <ugonzalez86@gmail.com>
2019-04-03 21:40:27 -0700

    This will commit all the recent project additions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,45 @@
-function Contact(first, last, email) {
+function User(first, last, email) {
   this.firstName = first;
   this.lastName = last;
-  this.firstEmail = email;
-
+  this.email = email;
+  this.carbs = [];
+  this.sugars = [];
 }
 
-Contact.prototype.fullContact = function() {
-  return this.firstName + " " + this.lastName + " " + this.firstEmail;
+User.prototype.fullContact = function() {
+  return this.firstName + " " + this.lastName + " " + this.email;
+}
+
+function Carbs(breakfast, lunch, dinner, snack) {
+  this.breakfast = breakfast;
+  this.lunch = lunch;
+  this.dinner = dinner;
+  this.snack = snack;
+}
+
+function Sugars(breakfast, lunch, dinner, snack) {
+  this.breakfast = breakfast;
+  this.lunch = lunch;
+  this.dinner = dinner;
+  this.snack = snack;
+}
+
+Carbs.prototype.carbTotal = function(){
+  if(this.breakfast + this.lunch + this.dinner + this.snack >= 181){
+    alert("You should consider lowering your daily carb intake! The recommended daily total amount is 180 grams. Each meal should have a total of 60 grams and snacks no more than 15 grams.")
+  } else if(this.breakfast + this.lunch + this.dinner + this.snack <= 44){
+    alert("You should consider increasing your daily carb intake. Each meal should have 15-60 grams of carbs and no more than 15 grams of carbs for snacks.")
+  } else {
+    alert("Great job with your carb counting!")
+  }
+}
+
+Sugars.prototype.sugarAlert = function(){
+  if(this.sugars >= 350){
+    alert("You're sugar is high, consider calling your doctor!")
+  } else if(this.sugars <= 70){
+    alert("You're sugar is low, consider calling your doctor!")
+  }
 }
 
 function resetFields() {
@@ -22,7 +55,8 @@ $("form#new-contact").submit(function(event) {
   var inputtedFirstName = $("input#new-first-name").val();
   var inputtedLastName = $("input#new-last-name").val();
   var inputtedFirstEmail = $("input#first-email").val();
-  var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedFirstEmail);
+  var newContact = new User(inputtedFirstName, inputtedLastName, inputtedFirstEmail);
+
 
   $("ul#contacts").append("<li><span class='contact'>" + newContact.fullContact() + "</span></li>");
 
@@ -33,9 +67,20 @@ $("form#new-contact").submit(function(event) {
     $("#show-contact h2").text(newContact.fullContact());
     $(".first-name").text(newContact.firstName);
     $(".last-name").text(newContact.lastName);
-    $(".first-email").text(newContact.firstEmail);
+    $(".first-email").text(newContact.email);
   });
 
   $("form#new-contact").not().css("display", "none");
   });
+
+
+  $('#log').click(function(){
+    var inputBCarbs = parseInt($('input#b-carbs').val());
+    var inputLCarbs = parseInt($('input#l-carbs').val());
+    var inputDCarbs = parseInt($('input#d-carbs').val());
+    var inputSCarbs = parseInt($('input#s-carbs').val());
+    var newTotalCarbs = new Carbs(inputBCarbs, inputLCarbs, inputDCarbs, inputSCarbs);
+
+    newTotalCarbs.carbTotal();
+  })
 })
